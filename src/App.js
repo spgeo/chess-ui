0814daa03eeeefae9ee627313d01619a783f8bee
Spiros Georgiladakis <sp.georgil@gmail.com>
2022-01-30 17:50:21 -0800

    Add demo and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Demo from './Demo';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,19 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Demo />
     </div>
   );
 }
